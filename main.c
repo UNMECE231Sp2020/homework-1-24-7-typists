2b663304b61563e3aa15b00bf39f7addbe3952e2
247Typing <laptop@Laptop.localdomain>
2020-02-05 11:35:01 -0700

    Added the required function pointer arrays and for loops to go throught them

--- main.c
+++ main.c
@@ -4,6 +4,14 @@
 int main() {
 	Complex a = {3, 4};
 	Complex b = {5, 12};
-
+	Complex (*complex_fn[4])(Complex, Complex) = {add, sub, mult, div};
+	double (*mag_phase_fn[2])(Complex) = {magnitude, phase};
+	int i;
+	for (i=0; i<4; i++){
+		complex_fn[i](a, b);
+	}
+	for (i=0; i<2; i++){
+		mag_phase_fn[i](a);
+	}
 	return 0;
 }
