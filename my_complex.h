553cd820f22bea3532e1a1a1b731cb6d2553e620
bradleyevans3 <bradleyevans3@gmail.com>
2020-02-05 10:53:39 -0700

    Added void twice. Added printf twice.

--- my_complex.h
+++ my_complex.h
@@ -9,6 +9,18 @@ typedef struct _complex Complex;
 
 void print_complex(Complex c);
 
+
+
+void hdlr1(Complex (*fn_hdlr) (Complex, Complex), Complex input_one, Complex input_two) {
+	printf("%lf %lf", fn_hdlr(input_one, input_two).real, fn_hdlr(input_one, input_two).imag);
+}
+
+void hdlr2(Double (*fn_hdlr) (Complex), Complex input_three) {
+	printf("%lf", fn_hdlr(input_three));
+}
+
+
+
 Complex complex_add(Complex c1, Complex c2);
 Complex complex_sub(Complex c1, Complex c2);
 Complex complex_mult(Complex c1, Complex c2);
